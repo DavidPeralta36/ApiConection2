52ad6c89bc14cd091166f80a3547c42345c886c0
David Peralta <19470212@campeche.tecnm.mx>
2023-05-25 20:24:42 -0600

    Agregar archivos de proyecto.

---
+++ ApiConection2/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiConection2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private HttpClient httpClient;
+
+        public HomeController()
+        {
+            httpClient = new HttpClient();
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7080/"); 
+
+                var response = await client.GetAsync("/api/values");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var values = await response.Content.ReadAsStringAsync();
+                    ViewBag.Values = values.Split(',');
+                }
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddValue(string newValue)
+        {
+            var apiUrl = "https://localhost:7080/api/values"; 
+
+            var content = new StringContent("\"" + newValue + "\"", Encoding.UTF8, "application/json");
+            var response = httpClient.PostAsync(apiUrl, content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response);
+                // Aquí puedes agregar la lógica en caso de que la solicitud sea exitosa
+            }
+            else
+            {
+                Console.WriteLine(response);
+                // Aquí puedes agregar la lógica en caso de que ocurra un error en la solicitud
+            }
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ ApiProyect/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProyect.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private static List<string> valuesList = new List<string> { "valor1", "valor22" };
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return valuesList;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] string newValue)
+        {
+            valuesList.Add(newValue);
+            Console.WriteLine(valuesList.Count());
+            return Ok();
+        }
+    }
+}
